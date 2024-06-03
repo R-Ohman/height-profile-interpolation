ae131b55af6a20f346f3ab88eafd5fd3fe4a17c2
Ruslan Rabadanov <vonadabarr@gmail.com>
2024-06-03 21:44:59 +0200

    Cosmetic changes

--- src/algorithms.py
+++ src/algorithms.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 def get_x_values(data: list[tuple[float, float]]) -> list[float]:
     """
-    Returns the x values from data.
+    Returns the first element of each tuple in data.
     :param data: list of tuples of x and y values
     :return: list: list of x values
     """
@@ -16,29 +16,31 @@ def get_x_values(data: list[tuple[float, float]]) -> list[float]:
 
 def get_y_values(data: list[tuple[float, float]]) -> list[float]:
     """
-    Returns the y values from data.
+    Returns the second element of each tuple in data.
     :param data: list of tuples of x and y values
     :return: list: list of y values
     """
     return [y for _, y in data]
 
 
-def get_selection_points(nodes: list[tuple[float, float]], strategy: Callable[[float, float, int], list[float]], n: int) -> list[tuple[float, float]]:
+def select_points_by_strategy(points: list[tuple[float, float]], strategy: Callable[[float, float, int], list[float]],
+                              n: int) -> list[tuple[float, float]]:
     """
     Returns the selection points based on the strategy.
-    :param nodes: list of tuples of x and y values
+    :param points: list of tuples of x and y values
     :param strategy: strategy to select the points (e.g., Chebyshev nodes)
     :param n: number of selection points
     :return: list: list of selection points
     """
-    correct_nodes = strategy(nodes[0][0], nodes[-1][0], n)
+    start_x, stop_x = points[0][0], points[-1][0]
+    strategy_x_values = strategy(start_x, stop_x, n)
 
-    # Find the closest nodes to correct_nodes from nodes.
     result = []
-    for node in correct_nodes:
-        closest_node = min(nodes, key=lambda x: abs(x[0] - node))
-        if closest_node not in result:
-            result.append(closest_node)
+    for x_value in strategy_x_values:
+        # Find the closest point to the x value, so we can get the y value in future
+        closest_point = min(points, key=lambda point: abs(point[0] - x_value))
+        if closest_point not in result:
+            result.append(closest_point)
     return result
 
 
@@ -52,7 +54,7 @@ def get_linspace_points(nodes: list[tuple[float, float]], n: int) -> list[float]
     return linspace(nodes[0][0], nodes[-1][0], n)
 
 
-def read_nodes(path: str) -> list[tuple[float, float]]:
+def read_data(path: str) -> list[tuple[float, float]]:
     """
     Read nodes from a CSV file.
     :param path: path to the CSV file
@@ -115,75 +117,69 @@ def cubic_spline_interpolation(data: list[tuple[float, float]], x_values: list[f
     :param x_values: list of x values to interpolate
     :return: list: list of interpolated y values
     """
+    A, b = create_matrix_and_vector(data)
+    coefficients = calculate_coefficients(A, b)
+    return interpolate_values(data, x_values, coefficients)
+
+
+def calculate_coefficients(A: Matrix, b: Matrix) -> list[float]:
+    return A.solve(b).flatten()
+
+
+def interpolate_values(data: list[tuple[float, float]], x_values: list[float], coefficients: list) -> list[float]:
     subranges_number = len(data) - 1
-    # the size of the matrix
+    results = []
+
+    for x_value in x_values:
+        found = False
+        for i in range(subranges_number):
+            if data[i][0] <= x_value <= data[i + 1][0]:
+                h = x_value - data[i][0]
+                y_interpolated = sum(coefficients[i * 4 + j] * h ** j for j in range(4))
+                results.append(y_interpolated)
+                found = True
+                break
+        if not found:
+            results.append(results[-1])
+    return results
+
+
+def create_matrix_and_vector(data: list[tuple[float, float]]) -> tuple[Matrix, Matrix]:
+    subranges_number = len(data) - 1
     n = 4 * subranges_number
 
     A = Matrix.create_matrix(n, n)
     b = Matrix.create_matrix(n, 1)
 
-    # the row index
     row_id = 0
 
-    # loop over each subrange
     for i in range(subranges_number):
-        # Set the first coefficient of the i-th cubic polynomial to 1
         A[row_id][i * 4] = 1
-        # Set the corresponding value in vector b to the y value of the i-th data point
         b[row_id] = data[i][1]
         row_id += 1
 
-        # Calculate the difference in x values between the i-th and (i+1)-th data points
         h = data[i + 1][0] - data[i][0]
-        # Set the coefficients of the i-th cubic polynomial for the (i+1)-th data point
         for j in range(4):
             A[row_id][i * 4 + j] = h ** j
-        # Set the corresponding value in vector b to the y value of the (i+1)-th data point
         b[row_id] = data[i + 1][1]
         row_id += 1
 
-        # If not the last subrange, set the conditions for the derivatives to be equal at the inner points
         if i < subranges_number - 1:
-            # Set the condition for the first derivatives to be equal
             for j in range(1, 4):
                 A[row_id][i * 4 + j] = j * h ** (j - 1)
             A[row_id][(i + 1) * 4 + 1] = -1
             row_id += 1
 
-            # Set the condition for the second derivatives to be equal
             A[row_id][i * 4 + 2] = 2
             A[row_id][i * 4 + 3] = 6 * h
             A[row_id][(i + 1) * 4 + 2] = -2
             row_id += 1
 
-    # Set the condition for the second derivative to be zero at the first point
     A[row_id][2] = 1
     row_id += 1
 
-    # Set the condition for the second derivative to be zero at the last point
     A[row_id][n - 2] = 2
     A[row_id][n - 1] = 6 * (data[-1][0] - data[-2][0])
     row_id += 1
 
-    # Solve the system of equations to find the coefficients of the cubic polynomials
-    x = A.solve(b).flatten()
-
-    # the list of results
-    results = []
-    # Loop over each x value to interpolate
-    for x_value in x_values:
-        found = False
-        # Find the subrange that the x value belongs to
-        for i in range(subranges_number):
-            if data[i][0] <= x_value <= data[i + 1][0]:
-                # Calculate the difference in x values
-                h = x_value - data[i][0]
-                # Calculate the interpolated y value using the coefficients of the i-th cubic polynomial
-                y_interpolated = sum(x[i * 4 + j] * h ** j for j in range(4))
-                results.append(y_interpolated)
-                found = True
-                break
-        # If the x value is outside the range of the data, use the last calculated y value
-        if not found:
-            results.append(results[-1])
-    return results
+    return A, b
